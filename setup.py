2100703958329ebfe5d2c914ac19e8b0f04eb1e9
Harsh Gupta <harshgupta204016@gmail.com>
2021-12-22 19:56:54 +0530

    adding setup file

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='extended_math',
+      version='0.0',
+      description='Mathmatical functions',
+      packages=['extended_math'],
+      author_email='harshgupta204016@gmail.com',
+      zip_safe=False)
