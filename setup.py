cadd68d68926ef26e421a163e1c587cd3c546225
Harsh Gupta <harshgupta204016@gmail.com>
2021-12-23 22:15:50 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,20 @@
 from setuptools import setup
+from setuptools import find_packages
 
-setup(name='extended_math',
-      version='0.0',
-      description='Mathmatical functions',
-      packages=['extended_math'],
-      author_email='harshgupta204016@gmail.com',
-      zip_safe=False)
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+if __name__ == "__main__":
+      
+      setup(
+            name='extended_maths',
+            version='0.0',
+            long_description=long_description,
+            long_description_content_type='text/markdown',
+            packages=find_packages(),
+            description='Mathmatical functions',
+            packages=['extended_maths'],
+            author_email='harshgupta204016@gmail.com',
+            url="https://github.com/harsh204016/extended-maths",
+            zip_safe=False
+      )
